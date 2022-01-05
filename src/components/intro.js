a9a0a984c2b5bfaa304d96e6471ab7e83eaffcab
Miguelito De Guzman <migueldeguzmandev@gmail.com>
2022-01-05 10:24:06 -0500

    Update intro.js
    Additional details

--- src/components/intro.js
+++ src/components/intro.js
@@ -4,10 +4,10 @@ export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        whitehatstoic.
+        whitehatstoic
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-         A stoic's way of learning cybersecurity.. 
+         A stoic learning cybersecurity and all things tech.. 
           (a statically generated website using{' '}
         <a
           href="https://www.gatsbyjs.com/"
