338f26987d9a5013819a888f288171218bc58f5e
Miguelito De Guzman <migueldeguzmandev@gmail.com>
2022-01-28 09:36:15 -0500

    Update intro.js
    Adding changes, to motto and links to websites

--- src/components/intro.js
+++ src/components/intro.js
@@ -7,20 +7,20 @@ export default function Intro() {
         whitehat_stoic
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-         Learning cybersecurity and all things tech.. 
-          (a statically generated website using{' '}
+         Thoughts on cybersecurity, tech and philosophy 
+          (check my other websites! {' '}
         <a
-          href="https://www.gatsbyjs.com/"
+          href="https://tech-stoic.github.io"
           className="underline hover:text-success duration-200 transition-colors"
         >
-          Gatsby
+          tech-stoic.github.io
         </a>{' '}
         and{' '}
         <a
-          href="https://www.datocms.com/"
+          href="https://www.rationalstoic.com/"
           className="underline hover:text-success duration-200 transition-colors"
         >
-          DatoCMS)
+          RationalStoic)
         </a>
         .
       </h4>
