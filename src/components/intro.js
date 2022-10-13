d2bb0502815bf7a0b9b937928e1c51befa656418
Miguelito De Guzman <migueldeguzmandev@gmail.com>
2022-10-13 15:15:27 -0500

    Update intro.js
    Change in Banner description

--- src/components/intro.js
+++ src/components/intro.js
@@ -7,7 +7,7 @@ export default function Intro() {
         whitehatStoic
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        Research, Tech, & Stoicism 
+        How A Stoic Breathes Life In The Modern Age... 
           (Check out our {' '}
         <a
           href="https://www.youtube.com/channel/UC8frhZgSux3uGmtOcACbcoQ"
