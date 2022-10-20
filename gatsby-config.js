4f10de761b94942c60d14a87bacd821af113641d
Miguelito De Guzman <migueldeguzmandev@gmail.com>
2022-10-20 12:28:42 -0500

    Update gatsby-config.js
    correction on google analytics id code

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,16 +11,19 @@ module.exports = {
       options: {
         apiToken: process.env.DATO_API_TOKEN,
         environment: process.env.DATO_ENVIRONMENT,
-     },
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
-        trackingId: "G-8TWESLQ9Z4",
-     },
+        },
+      },
     "gatsby-plugin-sharp",
     "gatsby-plugin-react-helmet",
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "G-8TWESLQ9Z4",
+     },
+    },
   ],
 };
