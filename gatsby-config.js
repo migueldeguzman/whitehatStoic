b75ebbfa163e6b0485546d69c318d9ddf5a3b9c9
Miguelito De Guzman <migueldeguzmandev@gmail.com>
2022-10-20 09:15:23 -0500

    Update gatsby-config.js
    added tracking for analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,12 @@ module.exports = {
         apiToken: process.env.DATO_API_TOKEN,
         environment: process.env.DATO_ENVIRONMENT,
       },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "G-8TWESLQ9Z4",
+     },
     },
     "gatsby-plugin-sharp",
     "gatsby-plugin-react-helmet",
