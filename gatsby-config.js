768094661cd8aa006b6e7982b538b9ee6b280c32
Miguelito De Guzman <migueldeguzmandev@gmail.com>
2022-10-20 12:10:59 -0500

    Update gatsby-config.js
    correcting google analytics code id

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,23 +2,22 @@ require("dotenv").config();
 
 module.exports = {
   siteMetadata: {
-    title: "whitehat stoic",
+    title: "whitehatStoic",
   },
   plugins: [
     "gatsby-plugin-postcss",
-    {
+     {
       resolve: "gatsby-source-datocms",
       options: {
         apiToken: process.env.DATO_API_TOKEN,
         environment: process.env.DATO_ENVIRONMENT,
-      },
-    {
+     },
+     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // replace "UA-XXXXXXXXX-X" with your own Tracking ID
         trackingId: "G-8TWESLQ9Z4",
      },
-    },
     "gatsby-plugin-sharp",
     "gatsby-plugin-react-helmet",
     `gatsby-plugin-image`,
