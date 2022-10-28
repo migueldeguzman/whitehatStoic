33f17bfb7afbc312c33ad9e16321a99bfaf4f335
Miguelito De Guzman <migueldeguzmandev@gmail.com>
2022-10-28 16:53:38 -0500

    Update intro.js
    updated header

--- src/components/intro.js
+++ src/components/intro.js
@@ -7,7 +7,7 @@ export default function Intro() {
         whitehatStoic
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A Stoic, Marathoner and Futurist Pursuing {' '}
+        Stoic, Marathoner and Futurist Pursuing {' '}
         <a
           href="https://en.wikipedia.org/wiki/Effective_altruism"
           className="underline hover:text-success duration-200 transition-colors"
